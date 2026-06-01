649dd35f7b38eb029eb0e04ca1a314cbdd223458
theubaidistan <ubaidistan395@gmail.com>
2026-06-01 12:23:42 +0500

    api fix

--- api/index.js
+++ api/index.js
@@ -1,49 +1,79 @@
 const fs = require('fs');
+const path = require('path');
 const url = require('url');
 const slugify = require('slugify');
+
 const replaceTemplate = require('../modules/replaceTemplate');
 
-const data = fs.readFileSync('./dev-data/data.json', 'utf-8');
+// ---------- SAFE FILE PATHS ----------
+const tempOverview = fs.readFileSync(
+  path.join(__dirname, '../templates/template-overview.html'),
+  'utf-8'
+);
+
+const tempCard = fs.readFileSync(
+  path.join(__dirname, '../templates/template-card.html'),
+  'utf-8'
+);
+
+const tempProduct = fs.readFileSync(
+  path.join(__dirname, '../templates/template-product.html'),
+  'utf-8'
+);
+
+const data = fs.readFileSync(
+  path.join(__dirname, '../dev-data/data.json'),
+  'utf-8'
+);
+
 const dataObj = JSON.parse(data);
+
+// optional slugs (not required but fine)
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
 
 module.exports = (req, res) => {
   const { pathname, query } = url.parse(req.url, true);
 
-  // OVERVIEW PAGE
+  // ---------------- OVERVIEW ----------------
   if (pathname === '/' || pathname === '/overview') {
-    const overview = fs.readFileSync(
-      './templates/template-overview.html',
-      'utf-8'
-    );
-    const card = fs.readFileSync('./templates/template-card.html', 'utf-8');
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
 
-    const cardsHtml = dataObj.map((el) => replaceTemplate(card, el)).join('');
-    const output = overview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
 
     res.setHeader('Content-Type', 'text/html');
     return res.end(output);
   }
 
-  // PRODUCT PAGE
+  // ---------------- PRODUCT ----------------
   if (pathname === '/product') {
-    const product = dataObj[query.id];
-    const productPage = fs.readFileSync(
-      './templates/template-product.html',
-      'utf-8'
-    );
+    if (!query.id) {
+      res.statusCode = 400;
+      return res.end('Missing product id');
+    }
 
-    const output = replaceTemplate(productPage, product);
+    const product = dataObj[query.id];
+
+    if (!product) {
+      res.statusCode = 404;
+      return res.end('Product not found');
+    }
+
+    const output = replaceTemplate(tempProduct, product);
 
     res.setHeader('Content-Type', 'text/html');
     return res.end(output);
   }
 
-  // API
+  // ---------------- API ----------------
   if (pathname === '/api') {
     res.setHeader('Content-Type', 'application/json');
     return res.end(JSON.stringify(dataObj));
   }
 
+  // ---------------- 404 ----------------
   res.statusCode = 404;
-  res.end('Not Found');
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Page not found!</h1>');
 };
